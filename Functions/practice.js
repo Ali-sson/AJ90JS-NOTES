a1ad81ec363deb3793b1cee90f0c3613b686d74c
James Ali <alissontech@gmail.com>
2023-10-23 10:40:17 -0700

    added js doc

---
+++ Functions/practice.js
@@ -0,0 +1,11 @@
+
+// converting Minutes to seconds 
+
+function MinToSec(Min){
+    let sec = (Min * 60);
+    return sec;
+}
+
+let Minutes = 60;
+let seconds = MinToSec(Minutes)
+console.log(` There are ${seconds} seconds in ${Minutes} Minutes`)
